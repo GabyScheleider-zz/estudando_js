17fe374c5cb5f1062188184d6b6fafec5a67bcc6
Gaby Scheleider <gabyscheleider@gmail.com>
2020-06-23 22:23:52 -0300

    Atualização 23.06.2020

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 60
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+if(vel > 60){//condição simples
+    console.log('Você ultrapassou a velocidade. MULTADO!')
+}
+console.log('Dirija com cuidado.')
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'EUA'
+console.log(`Nascendo no ${país}`)
+if(país != 'BRA'){
+    console.log('Você é estrangeiro')
+}else{
+    console.log('Você é brasileiro')
+}
